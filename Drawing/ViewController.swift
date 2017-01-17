0d4a4fcaf8e37ba31a64c228b0c685531857477e
Chihiro Kubota <rioneko4423@gmail.com>
2017-01-17 20:37:52 +0900

    Initial Commit

---
+++ Drawing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drawing
+//
+//  Created by 久保田千尋 on 2017/01/17.
+//  Copyright © 2017年 Chihiro Kubota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
